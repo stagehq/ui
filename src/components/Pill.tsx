ad29c5baf0af1acc9f49fb26c83f20e74e281d43
Felix Haeberle <kontakt@felixhaeberle.de>
2022-11-22 12:20:16 +0100

    add Pill

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-interface PillProps {
+export interface PillProps {
   name: string;
 }
 
---
+++ src/stories/Pill.stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Pill, PillProps } from '../components/Pill';
+
+export default {
+  title: 'UI/Pill',
+  component: Pill,
+} as Meta;
+
+const Template: Story<PillProps> = (args) => <Pill {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  name: "Text",
+};
