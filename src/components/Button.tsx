8161c7cf45912c31b04d9a87a704d7c542b20205
Nils Jacobsen <nils.jacobsen98@gmail.com>
2022-11-22 11:39:46 +0100

    added initial button setup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,9 @@
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
+export interface ButtonProps {
   primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
+  //needs to be changed to hero icon string type
+  icon?: string;
+  text: string;
+  link: string;
   onClick?: () => void;
 }
 
@@ -25,21 +11,18 @@ interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
+  primary = true,
+  icon = 'test',
+  text = 'Button Text',
+  link = 'https://getstage.app',
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
       {...props}
     >
-      {label}
+      {text}
     </button>
   );
 };
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,40 +1,21 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+// Button Story
 
-import { Button } from '../components/Button';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Button, ButtonProps } from '../components/Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'UI/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+} as Meta;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+  primary: false,
 };
