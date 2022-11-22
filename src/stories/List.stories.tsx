3ac2b18084511eea0fa8deb94a7587c6849afcb1
Felix Haeberle <kontakt@felixhaeberle.de>
2022-11-22 14:25:33 +0100

    add first List type card

--- src/stories/List.stories.tsx
+++ src/stories/List.stories.tsx
@@ -1,6 +1,7 @@
 // List story
 
 import { Story, Meta } from "@storybook/react/types-6-0";
+
 import { List, ListProps } from "../components/List";
 
 export default {
@@ -12,33 +13,10 @@ const Template: Story<ListProps> = (args) => <List {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  list: [
-    {
-      title: "Title",
-      description: "Description",
-      button: {
-        text: "Button",
-        link: "https://www.google.com",
-      },
-      date: "2021-01-01",
-    },
-    {
-      title: "Title",
-      description: "Description",
-      button: {
-        text: "Button",
-        link: "https://www.google.com",
-      },
-      date: "2021-01-01",
-    },
-    {
-      title: "Title",
-      description: "Description",
-      button: {
-        text: "Button",
-        link: "https://www.google.com",
-      },
-      date: "2021-01-01",
-    },
-  ],
+  type: "card",
+  title: "A bright view in the wide universe",
+  subtitle: "I once flew to the moon and back with a single rocket engine",
+  additional: "This is a List",
+  count: "324",
+  image: "https://source.unsplash.com/1000x500/?universe",
 };
