c5b877a2a67811e2edf3255f376bb0ab8bb560ae
Felix Haeberle <kontakt@felixhaeberle.de>
2022-12-13 13:28:20 +0100

    update export of PageHeader and PageFooter for internal usage

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,7 @@ import { Action } from "./components/Action";
 import { Cards } from "./components/Cards";
 import { Seperator } from "./components/Seperator";
 import { Header } from "./components/Header";
+import { PageHeader } from "./components/PageHeader";
+import { PageFooter } from "./components/PageFooter";
 
-export { Button, Section, List, Actions, Action, Cards, Seperator, Header };
+export { Button, Section, List, Actions, Action, Cards, Seperator, Header, PageFooter, PageHeader };
