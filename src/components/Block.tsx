5a990eb7a0abbbd30857f627f9dbd052c2aebb2d
Felix Haeberle <kontakt@felixhaeberle.de>
2023-01-06 14:22:30 +0100

    #4 format changes for better readability

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -7,75 +7,88 @@ import { Pills, PillsProps } from "./Pills";
 import { Seperator } from "./Seperator";
 
 export interface BlockProps {
-  children: React.ReactElement<ListProps | PillsProps | CardProps> | React.ReactElement<ListProps | PillsProps | CardProps>[];
+  children:
+    | React.ReactElement<ListProps | PillsProps | CardProps>
+    | React.ReactElement<ListProps | PillsProps | CardProps>[];
   actions?: React.ReactElement<ActionsProps>;
-  imagePath?: string
-  title?: string
-  subtitle?: string
-  size: number
-  isEditable: boolean
-  handleTitleChange?: (title: string) => void
-  handleSizeChange?: (size: number) => void
+  imagePath?: string;
+  title?: string;
+  subtitle?: string;
+  size: number;
+  isEditable: boolean;
+  handleTitleChange?: (title: string) => void;
+  handleSizeChange?: (size: number) => void;
 }
 
-export const Block = ({children, actions, imagePath, title, subtitle, size, isEditable, handleTitleChange, handleSizeChange}: BlockProps) => {
-
+export const Block = ({
+  children,
+  actions,
+  imagePath,
+  title,
+  subtitle,
+  size,
+  isEditable,
+  handleTitleChange,
+  handleSizeChange,
+}: BlockProps) => {
   const [titleState, setTitleState] = useState<string | undefined>(title);
   const [isHovering, setIsHovering] = useState<boolean>(false);
 
   const handleKeyPress = (e: KeyboardEvent) => {
-    if(e.code === "Enter"){
+    if (e.code === "Enter") {
       (e.target as HTMLInputElement).blur();
     }
-  }
+  };
 
   const handleSubmit = () => {
-    if(titleState != null && handleTitleChange != null){
+    if (titleState != null && handleTitleChange != null) {
       handleTitleChange(titleState);
-    }  
-  }
+    }
+  };
 
-  return <BlockEditWrapper isHovering={isHovering} setIsHovering={setIsHovering} size={size} handleSizeChange={handleSizeChange} isEditable={isEditable}>
-    <div 
-      className="@container flex flex-col gap-12 border border-zinc-200 bg-white shadow-sm rounded-2xl p-8 w-full"
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
+  return (
+    <BlockEditWrapper
+      isHovering={isHovering}
+      setIsHovering={setIsHovering}
+      size={size}
+      handleSizeChange={handleSizeChange}
+      isEditable={isEditable}
     >
-      <div className="flex flex-col gap-4">
-        <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-4 flex-grow">
-            {imagePath && <img src={imagePath} className="w-10 h-10 object-cover rounded-md"></img>}
-            <input
-              type="text"
-              name="title"
-              id="title"
-              className="block text-xl h-10 px-2 w-full rounded-md text-zinc-800 font-semibold focus:bg-zink-100 placeholder-transparent hover:placeholder-zinc-300 focus:border-black focus:ring-black focus:bg-white border-0"
-              placeholder="Enter title"
-              value={titleState}
-              onChange={(e) => setTitleState(e.target.value)}
-              onKeyDown={(e) => handleKeyPress(e)}
-              onBlur={() => handleSubmit()}
-            />
+      <div
+        className="@container flex flex-col gap-12 border border-zinc-200 bg-white shadow-sm rounded-2xl p-8 w-full"
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-4 flex-grow">
+              {imagePath && <img src={imagePath} className="w-10 h-10 object-cover rounded-md"></img>}
+              <input
+                type="text"
+                name="title"
+                id="title"
+                className="block text-xl h-10 px-2 w-full rounded-md text-zinc-800 font-semibold focus:bg-zink-100 placeholder-transparent hover:placeholder-zinc-300 focus:border-black focus:ring-black focus:bg-white border-0"
+                placeholder="Enter title"
+                value={titleState}
+                onChange={(e) => setTitleState(e.target.value)}
+                onKeyDown={(e) => handleKeyPress(e)}
+                onBlur={() => handleSubmit()}
+              />
+            </div>
+            <div className="w-10 h-10">{actions && actions}</div>
           </div>
-          <div className="w-10 h-10">
-          {actions && actions}
-          </div>
+          {subtitle && <p className="self-stretch text-sm text-left text-zinc-600 dark:text-zinc-400">{subtitle}</p>}
         </div>
-        {subtitle && <p className="self-stretch text-sm text-left text-zinc-600 dark:text-zinc-400">{subtitle}</p>}
+        <div className="flex flex-col gap-6">{children}</div>
       </div>
-      <div className="flex flex-col gap-6">
-        {children}    
-      </div>   
-    </div>
-  </BlockEditWrapper>
-}
+    </BlockEditWrapper>
+  );
+};
 
 Block.List = List;
 Block.Pills = Pills;
 Block.Card = Card;
 Block.Seperator = Seperator;
-
-
 
 // EditingHover ----------------------------------------
 interface BlockEditWrapperProps {
@@ -84,57 +97,86 @@ interface BlockEditWrapperProps {
   size: number;
   isEditable: boolean;
   setIsHovering: (value: boolean) => void;
-  handleSizeChange?: (size: number) => void
+  handleSizeChange?: (size: number) => void;
 }
 
-export const BlockEditWrapper:FC<BlockEditWrapperProps> = ({children, isHovering, size, isEditable, setIsHovering, handleSizeChange}) => {
-
+export const BlockEditWrapper: FC<BlockEditWrapperProps> = ({
+  children,
+  isHovering,
+  size,
+  isEditable,
+  setIsHovering,
+  handleSizeChange,
+}) => {
   const handleResize = (instruction: number) => {
-    if(size != instruction){
-      if (handleSizeChange != null){
+    if (size != instruction) {
+      if (handleSizeChange != null) {
         handleSizeChange(instruction);
       }
     }
-  }
+  };
 
-  return <div className={clsx("relative outline-2 outline-zinc-800 rounded-2xl cursor-grab w-full", isEditable && "hover:outline")}>
-    {children}
-    {isHovering && isEditable && 
-      <div 
-        className="absolute w-full flex justify-center h-11 -mt-5 gap-1" 
-        onMouseEnter={() => setIsHovering(true)} 
-        onMouseLeave={() => setIsHovering(false)}
-      >
-        <div className="flex gap-1 items-center px-2 bg-zinc-800 rounded-lg">
-          <div onClick={() => handleResize(3)} className={clsx("flex items-center justify-center w-10 h-8 hover:bg-zinc-700 rounded-md cursor-pointer", size === 3 ? "bg-zinc-600" : "bg-transparent")}>
-            <div className="border-2 border-white w-5 h-2 rounded-sm">
+  return (
+    <div
+      className={clsx(
+        "relative outline-2 outline-zinc-800 rounded-2xl cursor-grab w-full",
+        isEditable && "hover:outline"
+      )}
+    >
+      {children}
+      {isHovering && isEditable && (
+        <div
+          className="absolute w-full flex justify-center h-11 -mt-5 gap-1"
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+        >
+          <div className="flex gap-1 items-center px-2 bg-zinc-800 rounded-lg">
+            <div
+              onClick={() => handleResize(3)}
+              className={clsx(
+                "flex items-center justify-center w-10 h-8 hover:bg-zinc-700 rounded-md cursor-pointer",
+                size === 3 ? "bg-zinc-600" : "bg-transparent"
+              )}
+            >
+              <div className="border-2 border-white w-5 h-2 rounded-sm"></div>
+            </div>
+            <div
+              onClick={() => handleResize(2)}
+              className={clsx(
+                "flex items-center justify-center w-10 h-8 hover:bg-zinc-700 rounded-md cursor-pointer",
+                size === 2 ? "bg-zinc-600" : "bg-transparent"
+              )}
+            >
+              <div className="border-2 border-white w-4 h-4 rounded-sm"></div>
+            </div>
+            <div
+              onClick={() => handleResize(1)}
+              className={clsx(
+                "flex items-center justify-center w-10 h-8 hover:bg-zinc-700 rounded-md cursor-pointer",
+                size === 1 ? "bg-zinc-600" : "bg-transparent"
+              )}
+            >
+              <div className="border-2 border-white w-2 h-5 rounded-sm"></div>
             </div>
           </div>
-          <div onClick={() => handleResize(2)} className={clsx("flex items-center justify-center w-10 h-8 hover:bg-zinc-700 rounded-md cursor-pointer", size === 2 ? "bg-zinc-600" : "bg-transparent")}>
-            <div className="border-2 border-white w-4 h-4 rounded-sm">
-            </div>
-          </div>
-          <div onClick={() => handleResize(1)} className={clsx("flex items-center justify-center w-10 h-8 hover:bg-zinc-700 rounded-md cursor-pointer", size === 1 ? "bg-zinc-600" : "bg-transparent")}>
-            <div className="border-2 border-white w-2 h-5 rounded-sm">
-            </div>
+          <div className="flex items-center justify-center h-11 w-11 bg-zinc-800 hover:bg-zinc-700 text-white rounded-lg shadow-lg cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              ></path>
+            </svg>
           </div>
         </div>
-        <div className="flex items-center justify-center h-11 w-11 bg-zinc-800 hover:bg-zinc-700 text-white rounded-lg shadow-lg cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            className="w-6 h-6"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-            ></path>
-          </svg>
-        </div>
-      </div>}
-  </div>
-}
+      )}
+    </div>
+  );
+};
--- src/stories/Block.stories.tsx
+++ src/stories/Block.stories.tsx
@@ -13,27 +13,29 @@ export default {
 
 const exampleTitleHanlder = (title: string) => {
   console.log(title);
-}
+};
 
 const exampleChangeSize = (size: number) => {
   console.log("size: " + size);
-}
+};
 
 const Template: Story<BlockProps> = (args) => {
-  return <div className="flex flex-col gap-8 max-w-screen-xl px-24 mx-auto">
-    <div className="w-full">
-      <Block {...args} size={3} isEditable={true}/>
-    </div>
-    <div className="flex gap-8 flex-col md:flex-row">
-      <div className="w-full md:w-[calc((100%_-_32px)_/_2_*_1)] xl:w-[calc((100%_-_64px)_/_3_*_2_+_32px)]">
-        <Block {...args} size={2} isEditable={true}/>
+  return (
+    <div className="flex flex-col gap-8 max-w-screen-xl px-24 mx-auto">
+      <div className="w-full">
+        <Block {...args} size={3} isEditable={true} />
       </div>
-      <div className="w-full md:w-[calc((100%_-_32px)_/_2_*_1)] xl:w-[calc((100%_-_64px)_/_3_*_1)]">
-        <Block {...args} size={1} isEditable={true}/>
+      <div className="flex gap-8 flex-col md:flex-row">
+        <div className="w-full md:w-[calc((100%_-_32px)_/_2_*_1)] xl:w-[calc((100%_-_64px)_/_3_*_2_+_32px)]">
+          <Block {...args} size={2} isEditable={true} />
+        </div>
+        <div className="w-full md:w-[calc((100%_-_32px)_/_2_*_1)] xl:w-[calc((100%_-_64px)_/_3_*_1)]">
+          <Block {...args} size={1} isEditable={true} />
+        </div>
       </div>
     </div>
-  </div>
-}
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {
@@ -45,7 +47,9 @@ Default.args = {
   handleTitleChange: exampleTitleHanlder,
   handleSizeChange: exampleChangeSize,
   title: "Title",
-  children: <div className="p-4 bg-zinc-100 border border-zinc-200 flex items-center justify-center rounded-md h-32">Body</div>
+  children: (
+    <div className="p-4 bg-zinc-100 border border-zinc-200 flex items-center justify-center rounded-md h-32">Body</div>
+  ),
 };
 
 export const Blogs = Template.bind({});
@@ -59,17 +63,19 @@ Blogs.args = {
   handleSizeChange: exampleChangeSize,
   title: "Open Source",
   imagePath: "https://avatars.githubusercontent.com/u/65030610?s=200&v=4",
-  children: <Block.List>
-    {blogPosts.map((post, index) => (
-      <Block.List.Item
-        type={post.type}
-        title={post.title}
-        additional={post.additional}
-        subtitle={post.subtitle}
-        key={`Block-${index}`}
-      />
-    ))}
-  </Block.List>
+  children: (
+    <Block.List>
+      {blogPosts.map((post, index) => (
+        <Block.List.Item
+          type={post.type}
+          title={post.title}
+          additional={post.additional}
+          subtitle={post.subtitle}
+          key={`Block-${index}`}
+        />
+      ))}
+    </Block.List>
+  ),
 };
 
 export const Repos = Template.bind({});
@@ -83,18 +89,20 @@ Repos.args = {
   handleSizeChange: exampleChangeSize,
   title: "My Repositories",
   imagePath: "https://avatars.githubusercontent.com/u/9919?s=200&v=4",
-  children: <Block.List>
-    {openSource.map((project, index) => (
-      <Block.List.Item
-        type={project.type}
-        title={project.title}
-        additional={project.additional}
-        subtitle={project.subtitle}
-        count={project.count}
-        key={`Block-${index}`}
-      />
-    ))}
-  </Block.List>
+  children: (
+    <Block.List>
+      {openSource.map((project, index) => (
+        <Block.List.Item
+          type={project.type}
+          title={project.title}
+          additional={project.additional}
+          subtitle={project.subtitle}
+          count={project.count}
+          key={`Block-${index}`}
+        />
+      ))}
+    </Block.List>
+  ),
 };
 
 export const WebProjects = Template.bind({});
@@ -103,18 +111,20 @@ WebProjects.args = {
   handleSizeChange: exampleChangeSize,
   title: "Deployed Projects",
   imagePath: "https://avatars.githubusercontent.com/u/24602613?s=200&v=4",
-  children: <Block.List>
-    {projects.map((project, index) => (
-      <Block.List.Item
-        type={project.type}
-        title={project.title}
-        additional={project.additional}
-        subtitle={project.subtitle}
-        image={project.image}
-        key={`Block-${index}`}
-      />
-    ))}
-  </Block.List>
+  children: (
+    <Block.List>
+      {projects.map((project, index) => (
+        <Block.List.Item
+          type={project.type}
+          title={project.title}
+          additional={project.additional}
+          subtitle={project.subtitle}
+          image={project.image}
+          key={`Block-${index}`}
+        />
+      ))}
+    </Block.List>
+  ),
 };
 
 export const Music = Template.bind({});
@@ -128,18 +138,20 @@ Music.args = {
   handleSizeChange: exampleChangeSize,
   title: "Best Music ever",
   imagePath: "https://avatars.githubusercontent.com/u/251374?s=200&v=4",
-  children: <Block.List>
-    {spotify.map((track, index) => (
-      <Block.List.Item
-        type={track.type}
-        title={track.title}
-        subtitle={track.subtitle}
-        image={track.image}
-        index={index + 1}
-        key={`Block-${index}`}
-      />
-    ))}
-  </Block.List>
+  children: (
+    <Block.List>
+      {spotify.map((track, index) => (
+        <Block.List.Item
+          type={track.type}
+          title={track.title}
+          subtitle={track.subtitle}
+          image={track.image}
+          index={index + 1}
+          key={`Block-${index}`}
+        />
+      ))}
+    </Block.List>
+  ),
 };
 
 export const CV = Template.bind({});
@@ -153,16 +165,18 @@ CV.args = {
   handleSizeChange: exampleChangeSize,
   title: "Experience",
   imagePath: "https://avatars.githubusercontent.com/u/357098?s=200&v=4",
-  children: <Block.List>
-    {experience.map((pos, index) => (
-      <Block.List.Item
-        type={pos.type}
-        title={pos.title}
-        subtitle={pos.subtitle}
-        image={pos.image}
-        additional={pos.additional}
-        key={`Block-${index}`}
-      />
-    ))}
-  </Block.List>
-}
+  children: (
+    <Block.List>
+      {experience.map((pos, index) => (
+        <Block.List.Item
+          type={pos.type}
+          title={pos.title}
+          subtitle={pos.subtitle}
+          image={pos.image}
+          additional={pos.additional}
+          key={`Block-${index}`}
+        />
+      ))}
+    </Block.List>
+  ),
+};
