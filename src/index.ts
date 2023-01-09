6c37318a658d905788280cb3dde3a28c7a855037
Felix Haeberle <kontakt@felixhaeberle.de>
2023-01-09 15:54:53 +0100

    remove Action from export for release of 0.1.8

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { Button } from "./components/Button";
 import { Block } from "./components/Block";
 import { List } from "./components/List";
-import { Action } from "./components/Action";
 import { Card } from "./components/Card";
 import { Seperator } from "./components/Seperator";
 import { PageHeader } from "./components/PageHeader";
 import { PageFooter } from "./components/PageFooter";
 import { Pills } from "./components/Pills";
 
-export { Button, List, Action, Card, Seperator, Block, PageFooter, PageHeader, Pills };
+export { Button, List, Card, Seperator, Block, PageFooter, PageHeader, Pills };
