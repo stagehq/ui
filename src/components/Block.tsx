e6897396d71aa4e4850a976bbb48735b91205e67
NiklasBuchfink <niklas.buchfink@web.de>
2023-02-09 14:35:08 +0100

    fix Block Title hover & focus

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React, { KeyboardEvent, useEffect, useState } from "react";
 import { Action } from "./Action";
 import { ActionsProps } from "./Actions";
@@ -61,7 +62,9 @@ export const Block = ({
                 type="text"
                 name="title"
                 id="title"
-                className="block text-xl h-10 px-2 w-full rounded-md dark:bg-zinc-900 text-zinc-800 dark:text-zinc-100 font-semibold placeholder-transparent hover:placeholder-zinc-300 hover:bg-zinc-100 focus:bg-transparent dark:hover:bg-zinc-800 dark:hover:placeholder-zinc-300 dark:focus:bg-zinc-900 outline-none focus:ring-2 focus:ring-zinc-800 dark:focus:ring-zinc-100"
+                className={clsx("block text-xl h-10 px-2 w-full rounded-md dark:bg-zinc-900 text-zinc-800 dark:text-zinc-100 font-semibold placeholder-transparent bg-white border-0",
+                  isEditable && "hover:placeholder-zinc-300 dark:hover:placeholder-zinc-300 hover:bg-zinc-100 focus:bg-transparent focus:ring-black dark:focus:ring-white"
+                )}
                 placeholder="Enter title"
                 value={titleInternal}
                 onChange={(e) => setTitleInternal(e.target.value)}
